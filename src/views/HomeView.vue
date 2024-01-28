862674c0990c571e6a0e80a5c521dd43a25aa12f
Meteor owner <107754977+Meteor-owner@users.noreply.github.com>
2024-01-28 22:38:58 +0800

    1.28 version

---
+++ src/views/HomeView.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="header">
+    <header>
+      <div class="header-container">
+        <div class="nav-link">
+          <ul class="fll">
+            <router-link to="/">
+              <li>首页</li>
+            </router-link>
+            <router-link to="/houseList">
+              <li>房源</li>
+            </router-link>
+            <router-link to="/offer">
+              <li>发布房源</li>
+            </router-link>
+            <router-link to="/userPage">
+              <li>个人中心</li>
+            </router-link>
+          </ul>
+        </div>
+        
+      </div>
+    </header>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'HomeView',
+  components: {
+  }
+}
+</script>
+
+
+<style scoped>
+.ul{
+list-style: none;
+}
+</style>
